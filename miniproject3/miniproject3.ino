93a87554f0da1f48fd98d7c834a708714570e0c1
Anthony Costarelli <acostarelli@olin.edu>
2022-10-04 13:11:51 -0400

    okay

--- miniproject3/miniproject3.ino
+++ miniproject3/miniproject3.ino
@@ -1,14 +1,18 @@
 #include <Adafruit_MotorShield.h>
 Adafruit_MotorShield AFMS  = Adafruit_MotorShield(); 
 
-constexpr uint8_t ir_port  = 9;
-constexpr uint8_t ir_stbd = 10;
-Adafruit_DCMotor *motor_port = AFMS.getMotor(1);
-Adafruit_DCMotor *motor_stbd = AFMS.getMotor(2);
+constexpr uint8_t ir_port = A1;
+constexpr uint8_t ir_stbd = A0;
+Adafruit_DCMotor *motor_port = AFMS.getMotor(2);
+Adafruit_DCMotor *motor_stbd = AFMS.getMotor(1);
 
-constexpr uint8_t SPEED_STRAIGHT = 127;
-constexpr uint8_t SPEED_TURNING  = 200;
-constexpr uint8_t SEEING_BLACK = LOW;
+constexpr uint8_t SPEED_STRAIGHT = 25;
+constexpr uint8_t SPEED_TURNING  = 50;
+constexpr uint8_t SEEING_LINE = 850;
+
+bool seeingline(int ir) {
+  return ir >= SEEING_LINE;// && ir <= 950;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -21,11 +25,15 @@ void setup() {
     exit(1);
   }
 
-  motor_port->run(FORWARD);
-  motor_stbd->run(FORWARD);
+  motor_port->run(BACKWARD);
+  motor_stbd->run(BACKWARD);
 }
 
 void loop() {
-  motor_port->setSpeed(digitalRead(ir_stbd) == SEEING_BLACK ? SPEED_TURNING : SPEED_STRAIGHT);
-  motor_stbd->setSpeed(digitalRead(ir_port) == SEEING_BLACK ? SPEED_TURNING : SPEED_STRAIGHT);
+  Serial.print(analogRead(ir_port));
+  Serial.print(" ");
+  Serial.println(analogRead(ir_stbd));
+  motor_port->setSpeed(seeingline(analogRead(ir_stbd)) ? SPEED_TURNING : SPEED_STRAIGHT);
+  motor_stbd->setSpeed(SPEED_STRAIGHT);
+  //motor_stbd->setSpeed(analogRead(ir_port) > SEEING_LINE ? SPEED_TURNING : SPEED_STRAIGHT);
 }
